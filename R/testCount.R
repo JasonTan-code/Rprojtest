02092e446a3a74500a5aa322a0552f40b019f4cf
JasonTan-code <jason.taotaotan@gmail.com>
2021-02-02 14:38:49 -0500

    change dict

--- R/testCount.R
+++ R/testCount.R
@@ -4,7 +4,7 @@
 #' @export
 
 Calc_Count_Mean <- function(col_row){
-   count <- read.csv("./extdata/count.csv", row.names = 1)
+   count <- read.csv(system.file("extdata","count.csv",package = "cats"), row.names = 1)
    if (col_row == "col"){
      return(colMeans(count))
    } else if (col_row == "row"){
