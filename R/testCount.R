385112ae864970270ccd26dadebeec89b284f670
JasonTan-code <jason.taotaotan@gmail.com>
2021-02-02 14:22:52 -0500

    change

--- R/testCount.R
+++ R/testCount.R
@@ -2,11 +2,9 @@
 #'
 #' @return
 #' @export
-#'
-#' @examples
-#' Calc_Count_Mean("col")
+
 Calc_Count_Mean <- function(col_row){
-   count <- read.csv("./inst/extdata/count.csv", row.names = 1)
+   count <- read.csv("./extdata/count.csv", row.names = 1)
    if (col_row == "col"){
      return(colMeans(count))
    } else if (col_row == "row"){
